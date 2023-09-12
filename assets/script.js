d2ae15d9778cc604b728aeb6a876cf4acdcbadfa
Chaggar25 <142560645+Chaggar25@users.noreply.github.com>
2023-09-11 23:03:19 -0400

    added the script code from old repository that wasnt working, double checked if the code was working

---
+++ assets/script.js
@@ -0,0 +1,68 @@
+// Assignment code here
+var numericCharacters = ["1", "2", "3", "4", "5", "6", "7", "8", "9"];
+
+var uppercaseCharacters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+
+var lowercaseCharacters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
+var specialCharacters = ["@", "%", "+", "\\", "/", "'", "!", "#", "$", "^", "?", ":", ",", ")", "(", "}", "{", "]", "[", "~", "-", "_", "."];
+
+// Get references to the DOM elements
+var generateBtn = document.querySelector("#generate");
+var passwordText = document.querySelector("#password");
+
+// Function to prompt the user for the number of characters
+function getPasswordLength() {
+    var passwordLength = parseInt(prompt("How many characters do you want in your password?"));
+    
+    if (passwordLength < 8 || passwordLength > 128 || isNaN(passwordLength)) {
+        alert("Please choose a valid number of characters between 8 and 128.");
+        return getPasswordLength(); // Recursively ask for input until it's valid
+    }
+    
+    return passwordLength;
+}
+
+// Function to generate a password
+function generatePassword() {
+    var passwordLength = getPasswordLength();
+    var otherCharacters = [];
+
+    if (confirm("Do you want to include lowercase characters?")) {
+        otherCharacters = otherCharacters.concat(lowercaseCharacters);
+    }
+
+    if (confirm("Do you want to include uppercase characters?")) {
+        otherCharacters = otherCharacters.concat(uppercaseCharacters);
+    }
+
+    if (confirm("Do you want to include numeric characters?")) {
+        otherCharacters = otherCharacters.concat(numericCharacters);
+    }
+
+    if (confirm("Do you want to include special characters?")) {
+        otherCharacters = otherCharacters.concat(specialCharacters);
+    }
+
+    if (otherCharacters.length === 0) {
+        alert("You must select at least one character type.");
+        return generatePassword(); // Recursively ask for input until at least one character type is selected
+    }
+
+    var password = "";
+    for (var i = 0; i < passwordLength; i++) {
+        var randomIndex = Math.floor(Math.random() * otherCharacters.length);
+        password += otherCharacters[randomIndex];
+    }
+
+    return password;
+}
+
+// Function to display the generated password
+function writePassword() {
+    var password = generatePassword();
+    passwordText.value = password;
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
